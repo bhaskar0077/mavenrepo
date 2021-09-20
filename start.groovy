d132901a971d8696062bb7860641aee71d5ec7cd
bhaskar0077 <veebhar29@gmail.com>
2021-09-20 21:18:38 +0530

    test mvn repo

---
+++ start.groovy
@@ -0,0 +1,5 @@
+@Grab("group2737:spring-boot-starter-actuator:0.5.0.BUILD-SNAPSHOT")
+@Grab("group2737:spring-boot-starter-web:0.5.0.BUILD-SNAPSHOT")
+@ComponentScan("group2737.sample.service")
+class Start {
+}
